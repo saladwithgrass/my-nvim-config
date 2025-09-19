6abee59357499108b4b7331427c56dd172ca0f44
jazzhands <egod09080908@yandex.ru>
2025-09-19 20:50:00 +0300

    updated lspconfig and mappings

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,45 +1,50 @@
 return {
-  'neovim/nvim-lspconfig',
-  dependencies = { 'saghen/blink.cmp' },
+    'neovim/nvim-lspconfig',
+    dependencies = { 'saghen/blink.cmp' },
 
-  opts = {
-    servers = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            runtime = {
-              version = 'LuaJIT',
+    opts = {
+        servers = {
+
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = 'LuaJIT',
+                        },
+                        diagnostics = {
+                            globals = { "vim" }
+                        },
+                        workspace = {
+                            library = {
+                                vim.fn.expand "$VIMRUNTIME/lua",
+                                vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+                                vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+                                "${3rd}/luv/library",
+                            },
+                        },
+                    }
+                }
             },
-            diagnostics = {
-              globals = { "vim" }
+
+            clangd = {
+                cmd = {"clangd"},
+                filetypes = { 'c', 'cpp', 'h', 'hpp', 'ino'}
             },
-            workspace = {
-              library = {
-                vim.fn.expand "$VIMRUNTIME/lua",
-                vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
-                vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
-                "${3rd}/luv/library",
-              },
+
+            pyright = {
+                cmd = { "pyright-langserver", "--stdio" }
             },
-          }
+
+            texlab = {
+                cmd = {"texlab"}
+            }
+
         }
-      },
-      clangd = {
-        cmd = {"clangd"}
-      },
-      pyright = {
-        cmd = { "pyright-langserver", "--stdio" }
-      },
-      texlab = {
-        cmd = {"texlab"}
-      }
-    }
-  },
-  config = function(_, opts)
-    local lspconfig = require('lspconfig')
-    for server, config in pairs(opts.servers) do
-      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-      lspconfig[server].setup(config)
+    },
+    config = function(_, opts)
+        for server, config in pairs(opts.servers) do
+            config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+            vim.lsp.config(server, config)
+        end
     end
-  end
 }
